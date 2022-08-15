656f82f3c902f740aeaa946f3237a3203d89566d
SHREYA PRASAD <prasadshreya2303@gmail.com>
2022-08-15 14:49:04 +0530

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Shreya Prasad on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
