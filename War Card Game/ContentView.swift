00a7cfa8ba6a10e3fea3b45f7f2c09e380fb0d8f
SHREYA PRASAD <prasadshreya2303@gmail.com>
2022-08-20 22:24:42 +0530

    This is a fun card game.

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,12 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State  private var playerCard = "card6"
+    @State private  var cpucard = "card2"
+    @State  private var playerScore = 0
+    @State    private var cpuScore = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+        Image("background")
+            .ignoresSafeArea(.all)
+            
+            VStack{
+                Spacer()
+            Image("logo")
+                Spacer()
+            HStack{
+                Spacer()
+                Image(playerCard)
+                Spacer()
+                Image(cpucard)
+                Spacer()
+                
+            }
+                Spacer()
+                
+                Button(action: {
+                  //generate a random number between 2 to 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                  //update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpucard = "card" + String(cpuRand)
+                    
+                  //update the scores
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if  cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                    
+                } , label: {
+                    Image("dealbutton")
+                })
+                
+               
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                }
+                    Spacer()
+                    
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                }
+                    Spacer()
+        }
+                Spacer()
+                
+        
+            
     }
 }
-
+}
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
